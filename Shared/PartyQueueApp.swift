11b262591ca85f9e458e25f60eb5b7439a225696
Caleb <milkproducktionskirby@gmail.com>
2021-09-08 11:58:33 -0400

    Initial Commit

---
+++ Shared/PartyQueueApp.swift
@@ -0,0 +1,17 @@
+//
+//  PartyQueueApp.swift
+//  Shared
+//
+//  Created by Caleb Kirby on 9/8/21.
+//
+
+import SwiftUI
+
+@main
+struct PartyQueueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
